c38df49deb505242f9b2da7884881d69112cf79c
DaviGayDaSilva <languith54@gmail.com>
2026-02-10 12:21:04 -0300

    Create world.c for world generation and rendering

---
+++ src/world.c
@@ -0,0 +1,20 @@
+// world.c
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Function to generate a basic world
+void generate_world() {
+    printf("World generated!\n");
+}
+
+// Function to render the world
+void render_world() {
+    printf("World rendered!\n");
+}
+
+int main() {
+    generate_world();
+    render_world();
+    return 0;
+}
